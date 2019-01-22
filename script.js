9ca5feaf09c1f2155258110db0db7d191d10268e
zoraizhashmi <37843506+zoraizhashmi@users.noreply.github.com>
2019-01-22 14:44:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// "el" stands for the "element" that was just clicked
+function jimmyG(el){
+	el.classList.add("opaque");
+
+}
+
+// "el" stands for the "element" that was just clicked
+function busterP(el){
+	el.classList.add("opaque");
+}
+
+// "el" stands for the "element" that was just clicked
+function kobeB(el){
+	el.classList.add("opaque");
+
+}
+
+// "el" stands for the "element" that was just clicked
+function jumboJ(el){
+	el.classList.add("opaque");
+}
